2902f22a17cc8b33eadc3a594969d443306c4c28
Otavio Almeida <otavioalmeida650@gmail.com>
2023-08-12 20:15:22 -0300

    trying tesseract recognition

--- war_challenge_computer_vision/coordinates.py
+++ war_challenge_computer_vision/coordinates.py
@@ -4,5 +4,9 @@ coordinates = {
         "coord2": [384, 375],
         "color": [374, 357]
     },
-    
+    "Alemanha": {
+        "coord1": [506, 192],
+        "coord2": [525, 210],
+        "color": [515, 193]
+    }
 }
--- war_challenge_computer_vision/read_map.py
+++ war_challenge_computer_vision/read_map.py
@@ -1,34 +1,65 @@
 import cv2
+import numpy as np
 import pytesseract
+from PIL import Image, ImageOps
 
-from coordinates import coordinates
+from war_challenge_computer_vision.coordinates import coordinates
 
-image = cv2.imread('images/TelaDoJogo.png')
+image = Image.open("images/TelaDoJogo.png")
+
+
+def otsu_threshold(image, threshold):
+    # Convert the PIL image to a NumPy array
+    image_array = np.array(image)
+    # opencv_image = cv2.cvtColor(image_array, cv2.GRAY)
+    ret1, thresh = cv2.threshold(
+        image_array, threshold, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU
+    )
+
+    return Image.fromarray(cv2.cvtColor(thresh, cv2.COLOR_BGR2RGB))
+
 
 # Convert BGR to RGB (OpenCV loads images in BGR format)
-image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 for territory in coordinates.keys():
     top_left = coordinates[territory]["coord1"]
     bottom_right = coordinates[territory]["coord2"]
     c1, c2 = coordinates[territory]["color"]
-    print(c1, c2)
-    color = list(image[c2, c1].astype('int32'))
+    color = list(image.getpixel((c1, c2)))
 
     print(color)
 
     x1, y1 = top_left
     x2, y2 = bottom_right
-    
-    slice_image = image[y1: y2, x1 : x2]
 
-    troops_in_territory = pytesseract.image_to_string(slice_image)
-    print(f'There is {troops_in_territory} in {territory}')
-    print(image.shape)
-    cv2.rectangle(img=image, pt1=top_left, pt2=bottom_right, color=color, thickness=1)
+    slice_image = image.crop((x1, y1, x2, y2))
+    gray_image = slice_image.convert("L")
+    threshold = 140
+    threshold_image = otsu_threshold(gray_image, threshold)
+    image_contrast = ImageOps.autocontrast(
+        gray_image.point(lambda gray_pixel: 255 - gray_pixel)
+    ).resize((500, 500), resample=Image.BOX)
 
-# Display the image
-cv2.imshow('Square', slice_image)
-cv2.imshow('Square', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    high_resolution_threshold_image = threshold_image.resize(
+        (500, 500), resample=Image.BOX
+    )
+
+    # pil_image = Image.fromarray(cv2.cvtColor(slice_image, cv2.COLOR_BGR2GRAY))
+    # pil_image = Image.open("images/definicao-de-texto.png")
+
+    troops_in_territory = pytesseract.image_to_string(
+        high_resolution_threshold_image, lang="eng", config="--psm 6 outputbase digits"
+    )
+
+    print(f"There is {str(troops_in_territory).strip()} in {territory}")
+    # print(image.size)
+
+    # Display the image
+    # cv2.imwrite('images/Square_slice.png', slice_image)
+    high_resolution_threshold_image.save(f"images/{territory}_slice_threshold.png")
+    image_contrast.save(f"images/{territory}_contrast.png")
+    slice_image.save(f"images/{territory}_slice.png")
+    # cv2.imwrite('images/Square_rect.png', image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
