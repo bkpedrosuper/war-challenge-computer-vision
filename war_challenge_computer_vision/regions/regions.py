73945285b17f0556abd50fdc6e9937a308da12ad
Otavio Almeida <otavioalmeida650@gmail.com>
2023-08-13 18:12:26 -0300

    changing to singular name

--- war_challenge_computer_vision/regions/regions.py
+++ war_challenge_computer_vision/regions/regions.py
@@ -12,7 +12,7 @@ class ContinentData:
         self.idx = ContinentData.idx
 
 
-class Continents(Enum):
+class Continent(Enum):
     AFRICA = ContinentData(3)
     ASIA = ContinentData(7)
     EU = ContinentData(5)
@@ -24,57 +24,57 @@ class Continents(Enum):
 class RegionData:
     idx = 0
 
-    def __init__(self, continent: Continents):
+    def __init__(self, continent: Continent):
         RegionData.idx += 1
         self.continent = continent
         self.idx = RegionData.idx
 
 
-class Regions(Enum):
-    Brazil = RegionData(Continents.SA)
-    Argentina_Uruguay = RegionData(Continents.SA)
-    Colombia_Venezuela = RegionData(Continents.SA)
-    Peru_Bolivia_Chile = RegionData(Continents.SA)
+class Region(Enum):
+    Brazil = RegionData(Continent.SA)
+    Argentina_Uruguay = RegionData(Continent.SA)
+    Colombia_Venezuela = RegionData(Continent.SA)
+    Peru_Bolivia_Chile = RegionData(Continent.SA)
 
-    Mexico = RegionData(Continents.NA)
-    California = RegionData(Continents.NA)
-    New_York = RegionData(Continents.NA)
-    Labrador = RegionData(Continents.NA)
-    Ottawa = RegionData(Continents.NA)
-    Vancouver = RegionData(Continents.NA)
-    Mackenzie = RegionData(Continents.NA)
-    Alaska = RegionData(Continents.NA)
-    Greenland = RegionData(Continents.NA)
+    Mexico = RegionData(Continent.NA)
+    California = RegionData(Continent.NA)
+    New_York = RegionData(Continent.NA)
+    Labrador = RegionData(Continent.NA)
+    Ottawa = RegionData(Continent.NA)
+    Vancouver = RegionData(Continent.NA)
+    Mackenzie = RegionData(Continent.NA)
+    Alaska = RegionData(Continent.NA)
+    Greenland = RegionData(Continent.NA)
 
-    Iceland = RegionData(Continents.EU)
-    England = RegionData(Continents.EU)
-    Sweden = RegionData(Continents.EU)
-    Germany = RegionData(Continents.EU)
-    Spain_Portugal_France_Italy = RegionData(Continents.EU)
-    Poland_Yugoslavia = RegionData(Continents.EU)
-    Moscow = RegionData(Continents.EU)
+    Iceland = RegionData(Continent.EU)
+    England = RegionData(Continent.EU)
+    Sweden = RegionData(Continent.EU)
+    Germany = RegionData(Continent.EU)
+    Spain_Portugal_France_Italy = RegionData(Continent.EU)
+    Poland_Yugoslavia = RegionData(Continent.EU)
+    Moscow = RegionData(Continent.EU)
 
-    Algeria_Nigeria = RegionData(Continents.AFRICA)
-    Egypt = RegionData(Continents.AFRICA)
-    Congo = RegionData(Continents.AFRICA)
-    Sudan = RegionData(Continents.AFRICA)
-    Madagascar = RegionData(Continents.AFRICA)
-    South_Africa = RegionData(Continents.AFRICA)
+    Algeria_Nigeria = RegionData(Continent.AFRICA)
+    Egypt = RegionData(Continent.AFRICA)
+    Congo = RegionData(Continent.AFRICA)
+    Sudan = RegionData(Continent.AFRICA)
+    Madagascar = RegionData(Continent.AFRICA)
+    South_Africa = RegionData(Continent.AFRICA)
 
-    Middle_East = RegionData(Continents.ASIA)
-    Aral = RegionData(Continents.ASIA)
-    Omsk = RegionData(Continents.ASIA)
-    Dudinka = RegionData(Continents.ASIA)
-    Siberia = RegionData(Continents.ASIA)
-    Tchita = RegionData(Continents.ASIA)
-    Mongolia = RegionData(Continents.ASIA)
-    Vladivostok = RegionData(Continents.ASIA)
-    China = RegionData(Continents.ASIA)
-    India = RegionData(Continents.ASIA)
-    Japan = RegionData(Continents.ASIA)
-    Vietnam = RegionData(Continents.ASIA)
+    Middle_East = RegionData(Continent.ASIA)
+    Aral = RegionData(Continent.ASIA)
+    Omsk = RegionData(Continent.ASIA)
+    Dudinka = RegionData(Continent.ASIA)
+    Siberia = RegionData(Continent.ASIA)
+    Tchita = RegionData(Continent.ASIA)
+    Mongolia = RegionData(Continent.ASIA)
+    Vladivostok = RegionData(Continent.ASIA)
+    China = RegionData(Continent.ASIA)
+    India = RegionData(Continent.ASIA)
+    Japan = RegionData(Continent.ASIA)
+    Vietnam = RegionData(Continent.ASIA)
 
-    Borneo = RegionData(Continents.OCEANIA)
-    Sumatra = RegionData(Continents.OCEANIA)
-    New_Guinea = RegionData(Continents.OCEANIA)
-    Australia = RegionData(Continents.OCEANIA)
+    Borneo = RegionData(Continent.OCEANIA)
+    Sumatra = RegionData(Continent.OCEANIA)
+    New_Guinea = RegionData(Continent.OCEANIA)
+    Australia = RegionData(Continent.OCEANIA)
