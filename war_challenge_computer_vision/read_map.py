c9bccfd842c57241d40b21246f814cc6e1e42872
Otavio Almeida <otavioalmeida650@gmail.com>
2023-08-18 12:24:36 -0300

    abstracting main loop step to a function

--- war_challenge_computer_vision/read_map.py
+++ war_challenge_computer_vision/read_map.py
@@ -1,11 +1,10 @@
 import pytesseract
 import webcolors
-from PIL import Image
+from PIL.Image import Image as ImagePIL
 
-from war_challenge_computer_vision.coordinates import coordinates
+from war_challenge_computer_vision.coordinates import Coordinate
 from war_challenge_computer_vision.preprocessing.preprocessing import Preprocessor
-
-image = Image.open("images/TelaJogo.png")
+from war_challenge_computer_vision.regions.regions import Region
 
 
 def closest_colour(requested_colour: tuple[int, int, int]):
@@ -28,9 +27,7 @@ def get_colour_name(requested_colour: tuple[int, int, int]):
     return actual_name, closest_name
 
 
-image = image.resize((1920, 1080))
-
-for territory, coordinate in coordinates.items():
+def process_territory(image: ImagePIL, territory: Region, coordinate: Coordinate):
     top_left = coordinate.top_left
     bottom_right = (top_left[0] + 32, top_left[1] + 32)
     c1, c2 = (top_left[0] + 16, top_left[1] + 2)
@@ -38,7 +35,7 @@ for territory, coordinate in coordinates.items():
     color: tuple[int, int, int, int] = tuple(image.getpixel((c1, c2)))
 
     team_color, nearest_team_color = get_colour_name(color[:3])
-    print(f"{territory} Team Color {team_color} {nearest_team_color}")
+    # print(f"{territory} Team Color {team_color} {nearest_team_color}")
 
     x1, y1 = top_left
     x2, y2 = bottom_right
@@ -63,7 +60,9 @@ for territory, coordinate in coordinates.items():
         config="--psm 8 --oem 3 outputbase digits -c tessedit_char_whitelist=0123456789",
     )
 
-    print(f"There is {str(troops_in_territory).strip()} in {territory}")
+    # print(f"There is {str(troops_in_territory).strip()} in {territory}")
 
-    processed_image.save(f"images/{territory}_slice_threshold.png")
-    slice_image.save(f"images/{territory}_slice.png")
+    # processed_image.save(f"images/map_slices/{territory}_slice_threshold.png")
+    # slice_image.save(f"images/map_slices/{territory}_slice.png")
+
+    return (territory, int(troops_in_territory), nearest_team_color)
