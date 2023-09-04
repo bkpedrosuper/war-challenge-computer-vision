48413fabe710f04230296ff7a0f2996344c1524f
Otávio Almeida <otavioalmeida650@gmail.com>
2023-09-04 17:15:43 -0300

    refactoring color

---
+++ war_challenge_computer_vision/colors/territory_color.py
@@ -0,0 +1,20 @@
+import webcolors
+
+def closest_colour(requested_colour: tuple[int, int, int]):
+    min_colours: dict[int, str] = {}
+    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_colour[0]) ** 2
+        gd = (g_c - requested_colour[1]) ** 2
+        bd = (b_c - requested_colour[2]) ** 2
+        min_colours[(rd + gd + bd)] = name
+    return min_colours[min(min_colours.keys())]
+
+
+def get_colour_name(requested_colour: tuple[int, int, int]):
+    try:
+        closest_name = actual_name = webcolors.rgb_to_name(requested_colour)
+    except ValueError:
+        closest_name = closest_colour(requested_colour)
+        actual_name = None
+    return actual_name, closest_name
--- war_challenge_computer_vision/read_map.py
+++ war_challenge_computer_vision/read_map.py
@@ -1,31 +1,13 @@
 import pytesseract
-import webcolors
 from PIL.Image import Image as ImagePIL
+from war_challenge_computer_vision.colors.territory_color import get_colour_name
 
 from war_challenge_computer_vision.coordinates import Coordinate
 from war_challenge_computer_vision.preprocessing.preprocessing import Preprocessor
 from war_challenge_computer_vision.regions.regions import Region
 from war_challenge_computer_vision.utils.timer import timer_func
+from war_challenge_computer_vision.utils.enviroment import is_dev
 
-
-def closest_colour(requested_colour: tuple[int, int, int]):
-    min_colours: dict[int, str] = {}
-    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
-        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
-        rd = (r_c - requested_colour[0]) ** 2
-        gd = (g_c - requested_colour[1]) ** 2
-        bd = (b_c - requested_colour[2]) ** 2
-        min_colours[(rd + gd + bd)] = name
-    return min_colours[min(min_colours.keys())]
-
-
-def get_colour_name(requested_colour: tuple[int, int, int]):
-    try:
-        closest_name = actual_name = webcolors.rgb_to_name(requested_colour)
-    except ValueError:
-        closest_name = closest_colour(requested_colour)
-        actual_name = None
-    return actual_name, closest_name
 
 @timer_func
 def process_territory(image: ImagePIL, territory: Region, coordinate: Coordinate):
@@ -62,8 +44,8 @@ def process_territory(image: ImagePIL, territory: Region, coordinate: Coordinate
     )
 
     # print(f"There is {str(troops_in_territory).strip()} in {territory}")
-
-    # processed_image.save(f"images/map_slices/{territory}_slice_threshold.png")
-    # slice_image.save(f"images/map_slices/{territory}_slice.png")
+    if is_dev:
+        processed_image.save(f"images/map_slices/{territory}_slice_threshold.png")
+        slice_image.save(f"images/map_slices/{territory}_slice.png")
 
     return (territory, int(troops_in_territory), nearest_team_color)
